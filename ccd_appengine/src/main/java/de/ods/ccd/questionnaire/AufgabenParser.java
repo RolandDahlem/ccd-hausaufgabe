c184b342cb57a9489115a7346d4d07e7e2b72076
Roland Dahlem <roland.dahlem@ods-marketing.de>
2016-05-19 16:03:44 +0200

    Parser übersichtlicher gemacht

--- ccd_appengine/src/main/java/de/ods/ccd/questionnaire/AufgabenParser.java
+++ ccd_appengine/src/main/java/de/ods/ccd/questionnaire/AufgabenParser.java
@@ -7,39 +7,42 @@ import de.ods.ccd.questionnaire.domain.Aufgabe;
 
 class AufgabenParser {
 
-	private List<String> zeilen;
-
+	private final List<String> zeilen;
+	
+	private List<Aufgabe> aufgaben = new ArrayList<>();
+	private Aufgabe aktuelleAufgabe = null;
+	
 	public AufgabenParser(List<String> zeilen) {
 		this.zeilen = zeilen;
 	}
 
 	public List<Aufgabe> parse() {
-		
-		List<Aufgabe> aufgaben = new ArrayList<>();
-
-		Aufgabe aktuelleAufgabe = null;
 		for (int zeilennummer = 0; zeilennummer < zeilen.size(); zeilennummer++) {
-			String zeile = zeilen.get(zeilennummer);
-			if (zeile.startsWith("?")) {
-				if(aktuelleAufgabe != null){
-					aufgaben.add(aktuelleAufgabe);
-				}
-				aktuelleAufgabe = new Aufgabe();
-				
-				aktuelleAufgabe.setFrage(fragezeichenAnsEndeSetzen(zeile));
-			} else if (zeile.startsWith("*")) {
-				aktuelleAufgabe.setRichtigeAntwortIndex(zeilennummer -1);
-				aktuelleAufgabe.ergaenzeAntwortmoeglichkeit(zeile.substring(1));
-			} else {
-				aktuelleAufgabe.ergaenzeAntwortmoeglichkeit(zeile);
-			}
+			parseZeile(zeilennummer);
 		}
 
+		wechsleAufgabe();
+		return aufgaben;
+	}
+
+	private void parseZeile(int zeilennummer) {
+		String zeile = zeilen.get(zeilennummer);
+		if (zeile.startsWith("?")) {
+			wechsleAufgabe();
+			aktuelleAufgabe.setFrage(fragezeichenAnsEndeSetzen(zeile));
+		} else if (zeile.startsWith("*")) {
+			aktuelleAufgabe.setRichtigeAntwortIndex(zeilennummer -1);
+			aktuelleAufgabe.ergaenzeAntwortmoeglichkeit(zeile.substring(1));
+		} else {
+			aktuelleAufgabe.ergaenzeAntwortmoeglichkeit(zeile);
+		}
+	}
+
+	private void wechsleAufgabe() {
 		if(aktuelleAufgabe != null){
 			aufgaben.add(aktuelleAufgabe);
 		}
-		
-		return aufgaben;
+		aktuelleAufgabe = new Aufgabe();
 	}
 
 	private String fragezeichenAnsEndeSetzen(String zeile) {
