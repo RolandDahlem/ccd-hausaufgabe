8185bb0198b278ab920eedb504901caf21b170ee
Natascha Artamonova <Natalia.Artamonova@ods-marketing.de>
2016-06-06 10:59:09 +0200

    Refactoring: lesbarere Tests

--- ccd/src/test/java/de/ods/ccd/wecker/WeckerTest.java
+++ ccd/src/test/java/de/ods/ccd/wecker/WeckerTest.java
@@ -1,17 +1,21 @@
 package de.ods.ccd.wecker;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class WeckerTest {
@@ -22,6 +26,7 @@ public class WeckerTest {
 		
 		@Override
 		public void accept(String zeile) {
+			System.out.println(zeile);
 			ausgabe.add(zeile);
 		}
 
@@ -34,28 +39,31 @@ public class WeckerTest {
 		}
 		
 	}
-	
 
+	private MockDisplay ui;
+	private Wecker wecker;
+	
+	@Before
+	public void weckerVorbereiten(){
+		this.ui = new MockDisplay();
+		this.wecker = new Wecker();
+		wecker.setDisplay(ui);
+	}
 	
 	@Test
 	public void test_ob_die_weckzeit_gespeichert_werden_kann() throws Exception {
 
-		MockDisplay ui = new MockDisplay();
+		gibEin("9:30");
+		wecker.macheArbeit();
 		
-		Wecker wecker = new Wecker();
-		wecker.setDisplay(ui);
+		assertThat(ui.getLetzteAusgabe(), is("Weckzeit 9:30"));
+	}
 
-		String eingabe = "9:30";
-
+	private void gibEin(String eingabe) throws UnsupportedEncodingException, IOException {
 		InputStream inputstream = new ByteArrayInputStream(eingabe.getBytes(Charset.forName("UTF-8")));
 		InputStreamReader reader = new InputStreamReader(inputstream, "UTF-8");
 		BufferedReader bufferedReader = new BufferedReader(reader);
 		wecker.verarbeiteBenutzereingabe(bufferedReader);
-		
-		
-		wecker.macheArbeit();
-		
-		assertThat(ui.getLetzteAusgabe(), containsString("9:30"));
 	}
 
 }
