6929e605af93fa11ebe2c3186cab7adba5d9ee5f
Roland Dahlem <roland.dahlem@ods-marketing.de>
2016-07-11 15:06:53 +0200

    Rückgabe von JSON

--- ccd_appengine/src/main/java/de/ods/ccd/kanban/boars/KanbanInteractors.java
+++ ccd_appengine/src/main/java/de/ods/ccd/kanban/boars/KanbanInteractors.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import de.ods.ccd.kanban.contracts.Board;
@@ -17,9 +16,8 @@ public class KanbanInteractors {
 //	@Autowired
 //	private AufgabenFactory aufgabenFactory;
 		
-	@RequestMapping(value = "/kanban/start", method = RequestMethod.GET)
-	public @ResponseBody Board zeigeLeerenFragebogen(ModelMap model) throws IOException {
-
+	@RequestMapping("/kanban/start")
+	public @ResponseBody Board start(ModelMap model) throws IOException {
 		return new Board(null, null);
 	}
 	
