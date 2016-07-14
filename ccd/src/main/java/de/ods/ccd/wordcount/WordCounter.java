c243f7a2f2f979578aa958c7d8781677b58fd048
Roland Dahlem <roland.dahlem@ods-marketing.de>
2016-07-14 13:08:08 +0200

    WordCounterTest

---
+++ ccd/src/main/java/de/ods/ccd/wordcount/WordCounter.java
@@ -0,0 +1,41 @@
+package de.ods.ccd.wordcount;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordCounter {
+
+	public int countWords(String satz) {
+		
+		List<String> woerter = parseWoerter(satz);
+		return woerter.size();
+	}
+
+	private List<String> parseWoerter(String satz) {
+		List<String> woerter = new ArrayList<>();
+		
+		StringBuilder letztesWort = new StringBuilder();
+		for(int i=0; i<satz.length(); i++){
+			char zeichen = satz.charAt(i);
+			if(Character.isAlphabetic(zeichen)){
+				letztesWort.append(zeichen);
+			} else {
+				letztesWort = haengeWortAn(woerter, letztesWort);
+			}
+		}
+		
+		haengeWortAn(woerter, letztesWort);
+		return woerter;
+	}
+
+	private StringBuilder haengeWortAn(List<String> woerter, StringBuilder letztesWort) {
+		if(letztesWort.length() != 0){
+			woerter.add(letztesWort.toString());
+			letztesWort = new StringBuilder();
+		} else {
+			// z.B: 2 Leerezeichen hintereinander
+		}
+		return letztesWort;
+	}
+
+}
---
+++ ccd/src/test/java/de/ods/ccd/wordcount/WordCounterTest.java
@@ -0,0 +1,40 @@
+package de.ods.ccd.wordcount;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class WordCounterTest {
+
+	@Test
+	public void test_ob_woerter_in_einem_normalen_satz_gezaehlt_werden() {
+		WordCounter wordCounter = new WordCounter();
+		
+		assertThat(wordCounter.countWords("Mary had a little lamb"), is(5));
+	}
+
+	@Test
+	public void test_ob_woerter_bei_meheren_trennern_gezaehlt_werden() {
+		WordCounter wordCounter = new WordCounter();
+		
+		assertThat(wordCounter.countWords("Mary had a     little lamb"), is(5));
+	}
+	
+	@Ignore("Anforderungen ändern? :) ")
+	@Test
+	public void test_ob_woerter_bei_umlauten_gezaehlt_werden() {
+		WordCounter wordCounter = new WordCounter();
+		
+		assertThat(wordCounter.countWords("Märy had a little lamb"), is(6));
+	}
+	
+	@Test
+	public void test_ob_woerter_in_leeren_saetzen_gezaehlt_werden() {
+		WordCounter wordCounter = new WordCounter();
+		
+		assertThat(wordCounter.countWords(""), is(0));
+	}
+	
+}
