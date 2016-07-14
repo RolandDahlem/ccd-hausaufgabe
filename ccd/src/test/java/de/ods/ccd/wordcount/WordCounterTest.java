8e187e1b5b34e21c6abd2001e9417ed686518f7c
Roland Dahlem <roland.dahlem@ods-marketing.de>
2016-07-14 13:33:11 +0200

    refac

--- ccd/src/test/java/de/ods/ccd/wordcount/WordCounterTest.java
+++ ccd/src/test/java/de/ods/ccd/wordcount/WordCounterTest.java
@@ -8,39 +8,31 @@ import org.junit.Test;
 
 public class WordCounterTest {
 
+	private WordCounter wordCounter = new WordCounter();
+	
 	@Test
 	public void test_ob_woerter_in_einem_normalen_satz_gezaehlt_werden() {
-		WordCounter wordCounter = new WordCounter();
-		
 		assertThat(wordCounter.countWords("Mary had a little lamb"), is(5));
 	}
 
 	@Test
 	public void test_ob_woerter_bei_meheren_trennern_gezaehlt_werden() {
-		WordCounter wordCounter = new WordCounter();
-		
 		assertThat(wordCounter.countWords("Mary had a     little lamb"), is(5));
 	}
 	
 	@Test
 	public void test_ob_woerter_bei_zahlen_als_trenner_gezaehlt_werden() {
-		WordCounter wordCounter = new WordCounter();
-		
 		assertThat(wordCounter.countWords("Mary had a7little lamb"), is(5));
 	}
 	
 	@Ignore("Anforderungen ändern? :) ")
 	@Test
 	public void test_ob_woerter_bei_umlauten_gezaehlt_werden() {
-		WordCounter wordCounter = new WordCounter();
-		
 		assertThat(wordCounter.countWords("Märy had a little lamb"), is(6));
 	}
 	
 	@Test
 	public void test_ob_woerter_in_leeren_saetzen_gezaehlt_werden() {
-		WordCounter wordCounter = new WordCounter();
-		
 		assertThat(wordCounter.countWords(""), is(0));
 	}
 	
